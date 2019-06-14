f71e036ad76c0808ac9caae2dcca9ec8471c9aa2
Iván Hoffmann <digital.h@gmail.com>
2019-06-14 14:28:57 -0300

    Campo referente en Organización. Merjoando vista detalle de Organización

--- app/models/organizacion.js
+++ app/models/organizacion.js
@@ -2,6 +2,7 @@ import DS from "ember-data";
 
 export default DS.Model.extend({
   nombre: DS.attr("string"),
+  referente: DS.attr("string"),
   direccion: DS.attr("string"),
   localidad: DS.attr("string"),
   telefono: DS.attr("string"),
