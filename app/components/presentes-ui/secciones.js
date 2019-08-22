baa2234c48548c2a171dc309cc10a46e12f38a3f
Iván Hoffmann <digital.h@gmail.com>
2019-08-22 16:15:01 -0300

    Mejorando vista mobile

--- app/components/presentes-ui/secciones.js
+++ app/components/presentes-ui/secciones.js
@@ -7,14 +7,14 @@ export default Component.extend({
   tagName: "",
   activeClasses: computed("mobile", function() {
     if (this.mobile) {
-      return "bg-gray b";
+      return "bg-white gray b bb b-black";
     } else {
       return "bg-white gray b bb b-black";
     }
   }),
   clases: computed("mobile", function() {
     if (this.mobile) {
-      return "link pv2 ph3 m0 db w5 dim white";
+      return "link pv2 ph3 m0 db w-100 dim gray";
     } else {
       return "link h-100 pa2 pt3 m0 dib dim gray";
     }
