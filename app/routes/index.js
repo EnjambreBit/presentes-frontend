b8988574ba81d77c4e2497ae500b40734d245100
Iván Hoffmann <digital.h@gmail.com>
2019-10-22 16:43:24 -0300

    Fix en redireccionamiento de rutas

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,9 @@ import Route from "@ember/routing/route";
 export default Route.extend({
   afterModel() {
     if (window.location.href.includes("candidates2019.agenciapresentes.org")) {
-      return this.transitionTo("publico.index");
-    } else {
       return this.transitionTo("candidates2019");
+    } else {
+      return this.transitionTo("publico.index");
     }
   }
 });
