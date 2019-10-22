f9de347238058c2f2103b6e1b96d8c051219137f
Iván Hoffmann <digital.h@gmail.com>
2019-10-22 16:16:30 -0300

    Agregando textos y ajustando vista mobile

--- app/routes/candidates2019/index.js
+++ app/routes/candidates2019/index.js
@@ -5,9 +5,11 @@ export default Route.extend({
   actions: {
     mostrarModal(target) {
       $("#" + target).show();
+      $(".isotope-filter").hide();
     },
     cerrarModal(id) {
       $("#" + id).hide();
+      $(".isotope-filter").show();
     }
   }
 });
