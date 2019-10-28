f330570930e1ee9e1a703b248649c3ddbd628d37
Iván Hoffmann <digital.h@gmail.com>
2019-10-28 20:25:36 -0300

    Se agregan dos candidates y filtro por electe / no-electe

--- app/components/candidates-2019.js
+++ app/components/candidates-2019.js
@@ -12,49 +12,58 @@ export default Component.extend({
         percentPosition: true,
         transitionDuration: "0.7s",
         itemSelector: ".mini-ficha",
-        layoutMode: "fitRows"
+        layoutMode: "fitRows",
+        getSortData: {
+          resultado: ".resultado"
+        },
+        sortBy: "resultado"
       });
     });
 
-    $(".filter.identidades li a, .filter.partido-politico li a").click(
-      function() {
-        var classGroup = this.parentElement.parentElement.classList[1];
-        $(".filter." + classGroup + " li a").removeClass("active");
-        $(this).toggleClass("active");
+    $(
+      ".filter.identidades li a, .filter.partido-politico li a, .filter.resultados li a"
+    ).click(function() {
+      var classGroup = this.parentElement.parentElement.classList[1];
+      $(".filter." + classGroup + " li a").removeClass("active");
+      $(this).toggleClass("active");
 
-        var $this = $(this);
+      var $this = $(this);
 
-        var buttonGroup = $this.parents(".button-group");
-        var filterGroup = buttonGroup.attr("data-filter-group");
+      var buttonGroup = $this.parents(".button-group");
+      var filterGroup = buttonGroup.attr("data-filter-group");
 
-        filters[filterGroup] = $this.attr("data-filter");
+      filters[filterGroup] = $this.attr("data-filter");
 
-        if (filters["identidades"] || filters["partido-politico"]) {
-          $(".nota-candidates").hide();
-          if (
-            filters["identidades"] === "*" &&
-            filters["partido-politico"] === "*"
-          ) {
-            $(".nota-candidates").show();
-          } else {
-            $(".nota-candidates").hide();
-          }
-        }
-
-        var filterValue = concatValues(filters);
-
-        contenedor.isotope({
-          filter: filterValue
-        });
-        var elementosFiltrados = contenedor.isotope("getFilteredItemElements");
-        if (elementosFiltrados.length === 0) {
-          $(".grilla-vacia").fadeIn();
+      if (
+        filters["identidades"] ||
+        filters["partido-politico"] ||
+        filters["resultados"]
+      ) {
+        $(".nota-candidates").hide();
+        if (
+          filters["identidades"] === "*" &&
+          filters["partido-politico"] === "*" &&
+          filters["resultados"] === "*"
+        ) {
+          $(".nota-candidates").show();
         } else {
-          $(".grilla-vacia").fadeOut();
+          $(".nota-candidates").hide();
         }
-        return true;
       }
-    );
+
+      var filterValue = concatValues(filters);
+
+      contenedor.isotope({
+        filter: filterValue
+      });
+      var elementosFiltrados = contenedor.isotope("getFilteredItemElements");
+      if (elementosFiltrados.length === 0) {
+        $(".grilla-vacia").fadeIn();
+      } else {
+        $(".grilla-vacia").fadeOut();
+      }
+      return true;
+    });
     function concatValues(obj) {
       var value = "";
       for (var prop in obj) {
