b6595c8fcda8163e2077a5bb72159a12e269963e
Iván Hoffmann <digital.h@gmail.com>
2020-02-10 20:40:35 -0300

    Agregando filtrado por categoría en mapa. Mejorando componente.

--- app/components/presentes-ui/mapa.js
+++ app/components/presentes-ui/mapa.js
@@ -2,7 +2,16 @@ import Component from "@ember/component";
 
 export default Component.extend({
   tagName: "",
-
+  didInsertElement() {
+    let esPublico = false;
+    let ruta = "app.mapa";
+    let mapaPublico = this.get("mapaPublico");
+    if (mapaPublico === true) {
+      ruta = "publico.mapa";
+      esPublico = true;
+    }
+    return this.set("ruta", ruta);
+  },
   actions: {
     accionDePrueba(caso) {
       this.set("caso-numero", caso.id);
