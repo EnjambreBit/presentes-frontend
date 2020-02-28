4d9b695c72cf0fcdbbc4c162d15b91ec58b2ef4c
Iván Hoffmann <digital.h@gmail.com>
2020-02-28 11:28:22 -0300

    Ordenando estados mexicanos por referencia de matrimonio igualitario.

--- app/components/mapas/mapa-mexico.js
+++ app/components/mapas/mapa-mexico.js
@@ -1,43 +1,44 @@
 import Component from "@ember/component";
+import $ from "jquery";
 
 export default Component.extend({
   tagName: "",
   didInsertElement() {
-    Ember.$("path, circle").hover(function(e) {
+    $("path, circle").hover(function() {
       let provincia = "#LIST-" + this.id;
-      Ember.$("#info-box").css("display", "block");
-      Ember.$("#info-box").html(Ember.$(provincia).data("info"));
-      // Ember.$(provincia).css("color", "#d4145a");
-      Ember.$(provincia).toggleClass("red");
+      $("#info-box").css("display", "block");
+      $("#info-box").html($(provincia).data("info"));
+      // $(provincia).css("color", "#d4145a");
+      $(provincia).toggleClass("red");
     });
-    Ember.$("path, circle").mouseout(function(e) {
-      Ember.$("#info-box").css("display", "none");
+    $("path, circle").mouseout(function() {
+      $("#info-box").css("display", "none");
       let provincia = "#LIST-" + this.id;
-      // Ember.$(provincia).css("color", "rgba(0, 0, 0, 1)");
-      Ember.$(provincia).removeClass("red");
+      // $(provincia).css("color", "rgba(0, 0, 0, 1)");
+      $(provincia).removeClass("red");
     });
-    Ember.$(document).mousemove(function(e) {
+    $(document).mousemove(function(e) {
       $("#info-box").offset({
         left: e.pageX,
         top: e.pageY
       });
     });
-    Ember.$("ul li a").hover(function(e) {
+    $("ul li a").hover(function() {
       let itemId = this.id;
-      // Ember.$(this).css("color", "#d4145a");
-      Ember.$(this).toggleClass("red");
+      // $(this).css("color", "#d4145a");
+      $(this).toggleClass("red");
       let id = itemId.replace("LIST-", "");
-      Ember.$("path#" + id).toggleClass("provincia-hovered");
-      let path = Ember.$("path#" + id);
+      $("path#" + id).toggleClass("provincia-hovered");
+      let path = $("path#" + id);
       let position = path.offset();
-      Ember.$("#info-box").offset({ top: position.top, left: position.left });
-      Ember.$("#info-box").show();
-      Ember.$("#info-box").html(Ember.$(this).data("info"));
+      $("#info-box").offset({ top: position.top, left: position.left });
+      $("#info-box").show();
+      $("#info-box").html($(this).data("info"));
     });
-    Ember.$("ul li a").mouseout(function(e) {
-      Ember.$("#info-box").css("display", "none");
-      // Ember.$(this).css("color", "rgba(0, 0, 0, 1)");
-      Ember.$(this).removeClass("red");
+    $("ul li a").mouseout(function() {
+      $("#info-box").css("display", "none");
+      // $(this).css("color", "rgba(0, 0, 0, 1)");
+      $(this).removeClass("red");
     });
   }
 });
