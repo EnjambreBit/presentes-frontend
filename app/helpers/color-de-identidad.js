1d6d2fe87cea6c41403a0100e9d9e3ceb0971811
Iván Hoffmann <digital.h@gmail.com>
2021-04-28 08:43:38 -0300

    Fix en colores por identidad

--- app/helpers/color-de-identidad.js
+++ app/helpers/color-de-identidad.js
@@ -8,16 +8,16 @@ export function colorDeIdentidad(params /*, hash*/) {
   if (categoria === "lesbiana") {
     color = "purple b--purple";
   }
-  if ((categoria === "trans") || (categoria === "mujer-trans")) {
+  if ((categoria === "trans") || (categoria === "mujer-trans") || (categoria === "trans no-binarie")) {
     color = "green b--green";
   }
-  if (categoria === "gays") {
+  if (categoria === "gay") {
     color = "rojo b--rojo";
   }
   if ((categoria === "bisexual") || (categoria === "pansexual")) {
     color = "orange b--orange";
   }
-  if (categoria === "no-binarie") {
+  if ((categoria === "no-binarie") || (categoria === "no-binarie bisexual")) {
     color = "light-red b--light-red";
   }
 
