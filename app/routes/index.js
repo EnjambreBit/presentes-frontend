3ef17bca175b4d9566f324c2a04c73fbb6c88aa6
Iván Hoffmann <digital.h@gmail.com>
2023-08-04 16:54:10 -0300

    Ajustes en filtros y ruta agenciapresentes.org

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,8 @@ export default Route.extend({
       return this.transitionTo("candidates2019");
     } else if (window.location.href.includes("candidates-chile2021.agenciapresentes.org")) {
       return this.transitionTo("candidates-chile-2021");
+    } else if (window.location.href.includes("candidates2023.agenciapresentes.org")) {
+      return this.transitionTo("candidates2023");
     } else {
       return this.transitionTo("publico.index");
     }
